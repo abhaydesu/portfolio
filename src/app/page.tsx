2155a9d4fb0788bb56eb1cb0bbd13cb310c5a182
Abhay <sketchologistabhay@gmail.com>
2025-09-21 23:57:43 +0530

    feat: add text-flip component
    - Added a new `LayoutTextFlip` component to display rotating text.
    - Implemented animation using `motion` library for a smooth transition effect.
    - Updated `page.tsx` to utilize the new `LayoutTextFlip` component.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,15 +6,20 @@ import { Scales } from "@/components/scales";
 import { ProjectLanding } from "@/components/project-landing";
 import { Links } from "@/components/links";
 import TechShowcase from "@/components/tech-showcase";
+import { LayoutTextFlip } from "@/components/ui/layout-text-flip";
 
 export default function Home() {
   return (
    <div className="min-h-screen flex items-start justify-start">
     <Container className="min-h-screen pt-16 px-8 md:pt-20 md:pb-10">
       <Scales />
-      <Heading >
-        Abhay Singh
-      </Heading>
+      <LayoutTextFlip 
+        text="Abhay Singh"
+        words={["Full stack Developer", "Frontend Developer", "Software Engineer"]}
+      />
+      {/* <Heading > */}
+        {/* Abhay Singh
+      </Heading> */}
       <Subheading>
         i&#39;m a software engineer with a passion for building scalable and efficient systems. Currently a student, always a student of tech.
       </Subheading>
---
+++ src/components/ui/layout-text-flip.tsx
@@ -0,0 +1,66 @@
+"use client";
+import React, { useState, useEffect } from "react";
+import { motion, AnimatePresence } from "motion/react";
+import { cn } from "@/lib/utils";
+
+export const LayoutTextFlip = ({
+  text = "Build Amazing",
+  words = ["Landing Pages", "Component Blocks", "Page Sections", "3D Shaders"],
+  duration = 3000,
+}: {
+  text: string;
+  words: string[];
+  duration?: number;
+}) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % words.length);
+    }, duration);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <motion.div 
+        initial={{ opacity: 0, filter: "blur(10px)", y: 10}}
+        whileInView={{ opacity:1, filter: "blur(0px)", y:0}}
+        transition={{
+            duration:0.3,
+            ease: 'easeInOut'
+        }}
+        viewport={{once: true}}
+        className="px-4 flex md:items-center flex-col md:flex-row">
+      <motion.span
+        layoutId="subtext"
+        className="text-[var(--color-primary)] text-2xl font-bold tracking-tighter drop-shadow-md md:text-4xl"
+      >
+        {text}
+      </motion.span>
+
+      <motion.span
+        layout
+        className="relative w-fit md:mx-4 my-2 overflow-hidden rounded-md border border-transparent bg-white px-2 py-1 font-sans text-sm text-pink-400 shadow-sm ring shadow-black/10 ring-black/10 md:text-xs dark:bg-neutral-900 dark:shadow-sm dark:ring-1 dark:shadow-white/10 dark:ring-white/10"
+      >
+        <AnimatePresence mode="popLayout">
+          <motion.span
+            key={currentIndex}
+            initial={{ y: -10, filter: "blur(5px)" }}
+            animate={{
+              y: 0,
+              filter: "blur(0px)",
+            }}
+            exit={{ y: 10, filter: "blur(5px)", opacity: 0 }}
+            transition={{
+              duration: 0.3,
+            }}
+            className={cn("inline-block whitespace-nowrap")}
+          >
+            {words[currentIndex]}
+          </motion.span>
+        </AnimatePresence>
+      </motion.span>
+    </motion.div>
+  );
+};
